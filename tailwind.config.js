6923fe94497fab27774ae3d34652ea2f4aafad1e
Ertan Kayalar <ertankayalar@gmail.com>
2020-07-28 15:55:48 +0300

    custom body font

--- tailwind.config.js
+++ tailwind.config.js
@@ -92,7 +92,20 @@ module.exports = {
 
     },
     fontFamily: {
-      body: ['Nunito']
+      body: ['Nunito', 
+      'system-ui',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      '"Noto Sans"',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+      '"Noto Color Emoji"',]
     }
 
 
